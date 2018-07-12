a29f160cbb31c18ee5c7fb1aa13289031bdd1b6c
Sagun, Ashvani <ASAGUN@volvocars.com>
2018-07-12 17:47:32 +0530

    Routing with two components added

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,17 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MaterialModule} from './material.module';
 import { AppComponent } from './app.component';
-
-
+import { APIComponent } from './component/API/API.component';
+import { homeComponent } from './component/layout/home/home.component';
+import { NavMenuComponent } from './component/nav-menue/navmenu.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,APIComponent,homeComponent,NavMenuComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,MaterialModule
+    BrowserAnimationsModule,MaterialModule,
+    RouterModule.forRoot([
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'home', component: homeComponent},     
+      { path: 'API',component: APIComponent}      
+  ])
   ],
   providers: [],
   bootstrap: [AppComponent]
